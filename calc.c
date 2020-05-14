4bf5ded3a8b20557c2ebfbed32fb03abf9c604cf
mfcrespo <mafe_crespo@hotmail.com>
2020-05-14 20:51:35 +0000

    Add operations

--- calc.c
+++ calc.c
@@ -1,169 +1,118 @@
 #include "monty.h"
+
 /**
- *add - adds the top two elements of the stack
- *@stack: ptr to the top of the stack
- *@line_number: the line number of the command being run
- *
- *Return: void
+ * add - adds the top two elements of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
+ * Return: void
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	int sum;
-	int node = 0;
-	stack_t *iterator = *stack;
+	(void) stack;
+	(void) line_number;
 
-	while (iterator != NULL)
+	if (element->head != NULL && element->head->next != NULL)
 	{
-		node++;
-		iterator = iterator->next;
+		element->head->next->n = element->head->n + element->head->next->n;
+		pop(NULL, 0);
 	}
-	if (node < 2)
+	else
 	{
-		printf("L%u: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		exit_function(8);
 	}
-	sum = (*stack)->n + (*stack)->next->n;
-	(*stack)->next->n = sum;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
 }
 
 /**
- *sub - subtracts the top element of the stack from the second
- *top element of the stack.
- *@stack: ptr to the top of the stack
- *@line_number: the line number of the command being run
- *
- *Return: void
+ * sub - subtracts the top element of the stack from the second
+ * top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
+ * Return: void
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	int dif;
-	int node = 0;
-	stack_t *iterator = *stack;
+	(void) stack;
+	(void) line_number;
 
-	while (iterator != NULL)
+	if (element->head != NULL && element->head->next != NULL)
 	{
-		node++;
-		iterator = iterator->next;
+		element->head->next->n = element->head->next->n - element->head->n;
+		pop(NULL, 0);
 	}
-	if (node < 2)
+	else
 	{
-		printf("L%u: can't sub, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		exit_function(9);
 	}
-	dif = (*stack)->next->n - (*stack)->n;
-	(*stack)->next->n = dif;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
 }
 
 /**
- *divide - divides the second top element of the stack by
- *the top element of the stack.
- *@stack: ptr to the top of the stack
- *@line_number: the line number of the command being run
- *
- *Return: void
+ * divide - divides the second top element of the stack by the
+ * top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
+ * Return: void
  */
 void divide(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	int quotient;
-	int node = 0;
-	stack_t *iterator = *stack;
-
-	while (iterator != NULL)
+	(void) stack;
+	(void) line_number;
+	if (element->head != NULL && element->head->next != NULL)
 	{
-		node++;
-		iterator = iterator->next;
+		if (element->head->n == 0)
+			exit_function(13);
+		element->head->next->n = element->head->next->n / element->head->n;
+		pop(NULL, 0);
 	}
-	if (node < 2)
+	else
 	{
-		printf("L%u: can't div, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		exit_function(10);
 	}
-	if ((*stack)->n == 0)
-	{
-		printf("L%u: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	quotient = (*stack)->next->n / (*stack)->n;
-	(*stack)->next->n = quotient;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
 }
 
 /**
- *mul - multiplies the second top element of the
- *stack with the top element of the stack.
- *@stack: ptr to the top of the stack
- *@line_number: the line number of the command being run
- *
- *Return: void
+ * mul - multiplies the second top element of the stack
+ * with the top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
+ * Return: void
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	int product;
-	int node = 0;
-	stack_t *iterator = *stack;
+	(void) stack;
+	(void) line_number;
 
-	while (iterator != NULL)
+	if (element->head != NULL && element->head->next != NULL)
 	{
-		node++;
-		iterator = iterator->next;
+		element->head->next->n = element->head->n * element->head->next->n;
+		pop(NULL, 0);
 	}
-	if (node < 2)
+	else
 	{
-		printf("L%u: can't mul, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		exit_function(11);
 	}
-	product = (*stack)->n * (*stack)->next->n;
-	(*stack)->next->n = product;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
 }
 
 /**
- *mod - computes the rest of the division of the second top element of
- *the stack by the top element of the stack.
- *@stack: ptr to the top of the stack
- *@line_number: the line number of the command being run
- *
- *Return: void
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack
+ * @stack: ptr to the top of the stack
+ * @line_number: the line number of the command being execute
+ * Return: void
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp = *stack;
-	int module;
-	int node = 0;
-	stack_t *iterator = *stack;
+	(void) stack;
+	(void) line_number;
 
-	while (iterator != NULL)
+	if (element->head != NULL && element->head->next != NULL)
 	{
-		node++;
-		iterator = iterator->next;
+		if (element->head->n == 0)
+			exit_function(13);
+		element->head->next->n = element->head->next->n % element->head->n;
+		pop(NULL, 0);
 	}
-	if (node < 2)
+	else
 	{
-		printf("L%u: can't mod, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		exit_function(12);
 	}
-	if ((*stack)->n == 0)
-	{
-		printf("L%u: division by zero\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	module = (*stack)->next->n % (*stack)->n;
-	(*stack)->next->n = module;
-	*stack = (*stack)->next;
-	(*stack)->prev = NULL;
-	free(temp);
 }
